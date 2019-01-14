ed8cee3af059d709c00a3028515edb058f45c4b4
AL_ON_PC <alon.berrios@gmail.com>
2019-01-14 18:12:16 -0300

    React Router and Route implemented

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 import "./App.css";
 import logo from "./logo.png";
 import Launches from "./components/Launches";
+import LaunchItem from "./components/LaunchItem";
 
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql"
@@ -14,21 +16,24 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div className="App">
-          <img
-            className="logo"
-            src={logo}
-            alt="spacex"
-            style={{
-              width: 400,
-              display: "block",
-              margin: "auto",
-              marginTop: "1em",
-              marginBottom: "1em"
-            }}
-          />
-          <Launches />
-        </div>
+        <Router>
+          <div className="App">
+            <img
+              className="logo"
+              src={logo}
+              alt="spacex"
+              style={{
+                width: 400,
+                display: "block",
+                margin: "auto",
+                marginTop: "1em",
+                marginBottom: "1em"
+              }}
+            />
+            <Route exact path="/" component={Launches} />
+            <Route exact path="/launch/:flight_number" component={LaunchItem} />
+          </div>
+        </Router>
       </ApolloProvider>
     );
   }
---
+++ client/src/components/LaunchItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class LaunchItem extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Launch!</h1>
+      </div>
+    );
+  }
+}
+
+export default LaunchItem;
