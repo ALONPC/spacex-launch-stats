80203816fc750549bb6366858aabeacf7973146b
AL_ON_PC <alon.berrios@gmail.com>
2019-01-14 13:35:03 -0300

    React style added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import logo from "./logo.png";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <img
+          className="logo"
+          src={logo}
+          alt="spacex"
+          style={{
+            width: 400,
+            display: "block",
+            margin: "auto",
+            marginTop: "1em"
+          }}
+        />
       </div>
     );
   }
